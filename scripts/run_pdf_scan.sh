89d50c471cf70467fa6973ee3209e5fd36f0060c
Yannik Korzikowski <yannik@korzikowski.de>
2019-11-03 21:58:38 +0100

    added give number dir option

--- scripts/run_pdf_scan.sh
+++ scripts/run_pdf_scan.sh
@@ -7,13 +7,27 @@ do_pdf_filter() {
     ./pdf_filter.sh
 }
 
+give_number() {
+  number=$(/app/get_number.sh)
+  basename_=$(basename "$1"|sed 's/\.pdf//g')
+  dirname=$(dirname "$1")
+  path="${dirname}/../${number}_${basename_}.pdf"
+
+  mv $1 $path
+}
+
 export -f do_pdf_filter
+export -f give_number
+
+if ! [ -z ${GIVE_NUMBER_DIR+x} ] && ! [ -d /pdf/$GIVE_NUMBER_DIR ]; then
+  mkdir /pdf/$GIVE_NUMBER_DIR
+fi
 
 while [ true ]; do
   sleep 15
 
-  if [ -f /pdf/gimme ]; then
-    mv /pdf/gimme /pdf/"take_this $(/app/get_number.sh)"
+  if ! [ -z ${GIVE_NUMBER_DIR+x} ]; then
+    find "/pdf/$GIVE_NUMBER_DIR" -iname '*.pdf' -type f -exec bash -c 'give_number "$0"' {} \;
   fi
 
   find /filters/ -iname '*.env' -type f -exec bash -c 'do_pdf_filter "$0"' {} \;
